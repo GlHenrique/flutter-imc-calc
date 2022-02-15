9a4fc66032c98712bb060ac553903945bfbdb725
Glênio Carvalho <gleniocarvalho@MacBook-Pro-de-Glenio.local>
2022-02-15 00:38:38 -0300

    feat: add calc imc logic

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = 'Informe seus dados';
+
+  void resetForm() {
+    weightController.text = '';
+    heightController.text = '';
+    setState(() {
+      _infoText = 'Informe seus dados';
+    });
+    FocusManager.instance.primaryFocus?.unfocus();
+  }
+
+  void calculate() {
+    double weight = double.parse(weightController.text);
+    double height = double.parse(heightController.text) / 100;
+    double imc = weight / (height * height);
+    setState(() {
+      if (imc < 18.6) {
+        _infoText = 'Abaixo do peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = 'Peso ideal (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = 'Acima do peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = 'Obesidade grau I (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = 'Obesidade grau II (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 40) {
+        _infoText = 'Obesidade grau III (${imc.toStringAsPrecision(3)})';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +59,7 @@ class _HomeState extends State<Home> {
           backgroundColor: Colors.green,
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: resetForm,
               icon: const Icon(Icons.refresh),
             ),
           ],
@@ -40,26 +75,28 @@ class _HomeState extends State<Home> {
                 size: 120,
                 color: Colors.green,
               ),
-              const TextField(
+              TextField(
+                controller: weightController,
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Peso (kg)',
                   labelStyle: TextStyle(color: Colors.green),
                 ),
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   color: Colors.green,
                   fontSize: 14,
                 ),
               ),
-              const TextField(
+              TextField(
+                controller: heightController,
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Altura (cm)',
                   labelStyle: TextStyle(color: Colors.green),
                 ),
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   color: Colors.green,
                   fontSize: 14,
                 ),
@@ -72,7 +109,7 @@ class _HomeState extends State<Home> {
                     fontSize: 20,
                   ),
                 ),
-                onPressed: () {},
+                onPressed: calculate,
                 style: ElevatedButton.styleFrom(
                   primary: Colors.green,
                   padding: const EdgeInsets.all(16),
@@ -80,10 +117,10 @@ class _HomeState extends State<Home> {
               ),
               Container(
                 padding: const EdgeInsets.only(top: 16),
-                child: const Text(
-                  'Info',
+                child: Text(
+                  _infoText,
                   textAlign: TextAlign.center,
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: Colors.green,
                     fontSize: 25,
                   ),
